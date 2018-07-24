094078bf6e19e7f1acf83b4b90452a5d52456ba5
jpdaprato <jpdaprato@gmail.com>
2018-07-24 18:13:43 -0500

    Passing all tests for palindrome function

--- src/recursion.js
+++ src/recursion.js
@@ -124,6 +124,24 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  string = removeSpacesAndCaps(string);
+  let isPalindrome = string[0] === string[string.length - 1];
+  if (string.length <= 2) {
+    return isPalindrome;
+  }
+  return isPalindrome && palindrome(string.slice(1, -1));
+};
+
+const removeSpacesAndCaps = function(string) {
+  let noSpacesOrCaps = '';
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i]
+    if (string[i] !== ' ') {
+      char = string[i].toLowerCase();
+      noSpacesOrCaps += char;
+    }
+  }
+  return noSpacesOrCaps;
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
