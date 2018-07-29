6b73c0b9534db42088742548417d76a2bc4b078f
jpdaprato <jpdaprato@gmail.com>
2018-07-29 18:52:41 -0500

    Passing all tests for rMap function

--- src/recursion.js
+++ src/recursion.js
@@ -376,6 +376,12 @@ var countOccurrence = function(array, value) {
 // 21. Write a recursive version of map.
 // rMap([1,2,3], timesTwo); // [2,4,6]
 var rMap = function(array, callback) {
+  //let mappedArr = [];
+  if (array.length === 1) {
+    return [callback(array[0])];
+  }
+  let newArr = array.slice(1);
+  return [callback(array[0]), ...rMap(newArr, callback)];
 };
 
 // 22. Write a function that counts the number of times a key occurs in an object.
